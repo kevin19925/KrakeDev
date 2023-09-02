ee99d2de5448f2a687c0af3e00cf8cb998231b35
kevin19925 <kevinqui1_@hotmail.com>
2023-09-02 09:49:41 -0500

    reto 32

--- cap8/r25/saludar.js
+++ cap8/r25/saludar.js
@@ -3,6 +3,14 @@ return (document.getElementById(idComponente).value);
 }
 
 saludar=function(){
-let nombre=recuperar("txtNombre");
-let apellido=recuperar("txtApellido");
+let ingresos =recuperar("txtNombre");
+if (ingresos>=100 && ingresos<200) {
+    console.log("ok")
+    
+}
+//let nombre=recuperar("txtNombre");
+//let apellido=recuperar("txtApellido");
+
+
+
 }
