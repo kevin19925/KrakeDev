7f98057d4b569e46e9110d5c1dd76f4093d5a1b5
kevin19925 <kevinqui1_@hotmail.com>
2023-09-08 16:12:29 -0500

    reto 43

--- cap12/r43/r43.js
+++ cap12/r43/r43.js
@@ -20,15 +20,15 @@ crearProducto = function () {
     console.log("nombre proc2 : " + producto2.nombre);
 
     let p1 = producto1.cantidad;
-    let p2 = producto1.cantidad;
+    let p2 = producto2.cantidad;
     if (p1 == p2) { 
-        console.log("tienen igaul stock " );
+        console.log("tienen igual stock " );
     }
     if (p1 > p2) {
-        console.log("producto 1 tienen mas stock" );
+        console.log("producto 1 tiene mas stock" );
     }
     if (p2 > p1) {
-        console.log("producto 2 tienen mas stock" );
+        console.log("producto 2 tiene mas stock" );
     }
 
 
