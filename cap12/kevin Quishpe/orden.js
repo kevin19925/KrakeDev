167f4cd841db45a47ddb0a21dea09a564c04de35
kevin19925 <kevinqui1_@hotmail.com>
2023-09-25 22:27:47 -0500

    modulo 1 modificado

--- cap12/kevin Quishpe/orden.js
+++
@@ -1,7 +0,0 @@
-let personas = [
-    {  nombre: "marcos",edad: 18 },
-    {  nombre: "roberto",edad: 15 },
-    {  nombre: "kate",edad: 25 },
-    {  nombre: "diana",edad: 12 },
-    {  nombre: "benja",edad: 5 },
-]
--- cap9/r32/PormedioNotas/serviciosPromedioNotas.js
+++
@@ -1,4 +0,0 @@
-calcularPromedio=function(n1,n2,n3){
-
-return parseFloat((n1+n2+n3)/3).toFixed(2);
-}
