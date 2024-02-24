c6497aa169c75f9305021e6f22ddc27a8aad5582
kevin19925 <kevinqui1_@hotmail.com>
2024-02-24 13:25:27 -0500

    r

--- modulo 4/individual/cap2_consumir_rws/contactos1/rest_cliente/contact.js
+++ modulo 4/individual/cap2_consumir_rws/contactos1/rest_cliente/contact.js
@@ -35,3 +35,40 @@ export const saveContactRest = (contact, fnshowMessage) => {
       console.log(body);
     });
 };
+
+export const updateRest = (contact, fnshowMessage) => {
+  const config = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // la estrctura que espera el rws el objeto
+    body: JSON.stringify({
+      nombre: contact.nom,
+      apellido: contact.ape,
+      telefono: contact.tele,
+    }),
+  };
+  fetch(url + "contactos/" + contact.id, config)
+    .then((res) => {
+      return res.json();
+    })
+    .then((body) => {
+      fnshowMessage("Se actualizo");
+      console.log(body);
+    });
+};
+
+export const deleteRest = (contact, fnshowMessage) => {
+  const config = {
+    method: "DELETE",
+  };
+  fetch(url + "contactos/" + contact.id, config)
+    .then((res) => {
+      return res.json();
+    })
+    .then((body) => {
+      fnshowMessage("Se elimino ");
+      console.log(body);
+    });
+};
